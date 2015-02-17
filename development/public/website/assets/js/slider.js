31284bacf8a4f5bbe87ff01af76cb977a000ec1b
dario-c <dariocavanillas@gmail.com>
2015-02-17 14:09:14 +0100

    Added real Images and JS functions to append them

--- development/public/website/assets/js/slider.js
+++ development/public/website/assets/js/slider.js
@@ -12,6 +12,8 @@
     ns.Slider = function ()
     {
 
+      var imagesRoot = "url(website/assets/images/";
+
       var posibleXPositions = ["rightest", "right", "middle", "left", "leftest" ];
       var posibleYPositions = ["topmost", "top", "center", "bottom", "bottommost"];
 
@@ -23,21 +25,19 @@
       
       var columns = [col1, col2, col3, col4, col5];
 
-      var row1 = {name: "row-1", posClass: "topmost"};
-      var row2 = {name: "row-2", posClass: "top"};
-      var row3 = {name: "row-3", posClass: "center"};
-      var row4 = {name: "row-4", posClass: "bottom"};
-      var row5 = {name: "row-5", posClass: "bottommost"};
+      var row1 = {name: "row-1", posClass: "topmost",    artist: "CraigKarl",   extension: ".jpg"};
+      var row2 = {name: "row-2", posClass: "top",        artist: "DanMatutina", extension: ".jpg"};
+      var row3 = {name: "row-3", posClass: "center",     artist: "JamieCullen", extension: ".jpg"};
+      var row4 = {name: "row-4", posClass: "bottom",     artist: "JMTixier",    extension: ".jpg"};
+      var row5 = {name: "row-5", posClass: "bottommost", artist: "RobinDavey",  extension: ".gif"};
       
       var rows = [row1, row2, row3, row4, row5];
 
        function slide(columnOrRow, posiblePositions, positive) {
          // Loop through all Columns or Rows
           for(var x = 0; x < columnOrRow.length; x++) {
-            
             // Update elements attributes
             updateElement(columnOrRow[x], posiblePositions, positive);
-            
             // Apply changes to posClasses
             $("."+columnOrRow[x].name)
               .addClass(columnOrRow[x].posClass)
@@ -59,13 +59,23 @@
           } else if(positive && newPosition < 0) {
 
               columnOrRow.posClass = posiblePositions[posiblePositions.length - 1];
-          
+
           } else {
               columnOrRow.posClass = posiblePositions[newPosition];
           }
       }
 
+      function appendImages(){
+        for(var x = 0; x < rows.length; x++){
+            for(var y = 0; y < columns.length; y++ ){
+            $("." + rows[x].name + "." + columns[y].name).css({ "background-image": imagesRoot + rows[x].artist + "-0" + (y+2) + rows[x].extension + ")"});
+            }
+        }
+      }
+
         var init = function (){
+          appendImages();
+
           var animating = false;
 
           var controller = new Leap.Controller();
@@ -73,11 +83,12 @@
           controller.on("frame", function(frame){
             if(frame.hands.length > 0){
               var hand = frame.hands[0];
+              
               var translation = hand.translation(controller.frame(10));
               var translationX = translation[0];
               var translationY = translation[1];
               
-              var translationThreshold = 200;
+              var translationThreshold = 175;
 
                 var horizontal = Math.abs(translationX) > Math.abs(translationY) ? true : false;
 
