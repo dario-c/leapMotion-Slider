4abacddacb626c4ec239c9d0d2a476d92ca7024f
dario-c <dariocavanillas@gmail.com>
2015-02-18 16:23:26 +0100

    Improved JS Structure and added background to body

--- development/public/website/assets/js/slider.js
+++ development/public/website/assets/js/slider.js
@@ -39,135 +39,131 @@
 
       var slide = function(columnOrRow, posiblePositions, positive) {
          // Loop through all Columns or Rows
-          for(var x = 0; x < columnOrRow.length; x++) {
-            // Update elements attributes
-            updateElement(columnOrRow[x], posiblePositions, positive);
-            // Apply changes to posClasses
-            $("."+columnOrRow[x].name)
-              .addClass(columnOrRow[x].posClass)
-              .removeClass(columnOrRow[x].oldClass);
-          }
-       };
+        for(var x = 0; x < columnOrRow.length; x++) {
+          // Update elements attributes
+          updateElement(columnOrRow[x], posiblePositions, positive);
+          // Apply changes to posClasses
+          $("."+columnOrRow[x].name)
+            .addClass(columnOrRow[x].posClass)
+            .removeClass(columnOrRow[x].oldClass);
+        }
+      };
 
       var updateElement = function(columnOrRow, posiblePositions, positive){
+        var oldPosition = posiblePositions.indexOf(columnOrRow.posClass);
+        var newPosition = positive ? oldPosition - 1 : oldPosition + 1;
 
-          var oldPosition = posiblePositions.indexOf(columnOrRow.posClass);
-          var newPosition = positive ? oldPosition - 1 : oldPosition + 1;
+        columnOrRow.oldClass = columnOrRow.posClass;
 
-          columnOrRow.oldClass = columnOrRow.posClass;
+        if(!positive && newPosition >= posiblePositions.length) {
 
-          if(!positive && newPosition >= posiblePositions.length) {
+          columnOrRow.posClass = posiblePositions[0];
 
-              columnOrRow.posClass = posiblePositions[0];
+        } else if(positive && newPosition < 0) {
 
-          } else if(positive && newPosition < 0) {
+          columnOrRow.posClass = posiblePositions[posiblePositions.length - 1];
 
-              columnOrRow.posClass = posiblePositions[posiblePositions.length - 1];
-
-          } else {
-              columnOrRow.posClass = posiblePositions[newPosition];
-          }
+        } else {
+          columnOrRow.posClass = posiblePositions[newPosition];
+        }
       };
 
       var appendImages = function(){
         for(var x = 0; x < rows.length; x++){
-            for(var y = 0; y < columns.length; y++ ){
-            $("." + rows[x].name + "." + columns[y].name).css({ "background-image": "url(" + imagesRoot + rows[x].artist + "-0" + (y+2) + rows[x].extension + ")"});
+          for(var y = 0; y < columns.length; y++ ){
+          $("." + rows[x].name + "." + columns[y].name).css({ "background-image": "url(" + imagesRoot + rows[x].artist + "-0" + (y+2) + rows[x].extension + ")"});
+          }
+        }
+      };
+
+      function animate(direction) {
+        switch(direction){
+          case left:
+            slide(columns, posibleXPositions, false); // left
+            break;
+          case up:
+            slide(rows, posibleYPositions, true); // up
+            break;
+          case right:
+            slide(columns, posibleXPositions, true); // right
+            break;
+          case down:
+            slide(rows, posibleYPositions, false); // down
+            break;
+        }
+        animating = true;
+
+        window.setTimeout(function(){
+          animating = false;
+        }, 500);
+
+      }
+
+      var animating = false;
+      var controller = new Leap.Controller();
+
+      var processFrame = function(frame){
+        if(frame.hands.length > 0){
+          var hand = frame.hands[0];
+          
+          var translation = hand.translation(controller.frame(10));
+          var translationX = translation[0];
+          var translationY = translation[1];
+          
+          var translationThreshold = 175;
+
+          var horizontal = Math.abs(translationX) > Math.abs(translationY) ? true : false;
+
+          if(!animating){
+
+            if(horizontal) {
+
+              if(translationX < -translationThreshold) {
+                animate(left);
+              } else if (translationX > translationThreshold)  {
+                animate(right);
+              }
+            } else {
+
+              if(translationY < -translationThreshold) {
+                animate(down);
+              } else if (translationY > translationThreshold ) {
+                animate(up);
+              }
             }
+          }
         }
       };
 
       var init = function (){
-
         appendImages();
 
-        var animating = false;
+        controller.on("frame", processFrame);
+        controller.connect();
 
-        var controller = new Leap.Controller();
-
-        controller.on("frame", function(frame){
-          if(frame.hands.length > 0){
-            var hand = frame.hands[0];
-            
-            var translation = hand.translation(controller.frame(10));
-            var translationX = translation[0];
-            var translationY = translation[1];
-            
-            var translationThreshold = 175;
-
-              var horizontal = Math.abs(translationX) > Math.abs(translationY) ? true : false;
-
-                  if(!animating){
-
-                    if(horizontal) {
-
-                      if(translationX < -translationThreshold) {
-                        animate(left);
-                      } else if (translationX > translationThreshold)  {
-                        animate(right);
-                      }
-                    } else {
-
-                      if(translationY < -translationThreshold) {
-                        animate(down);
-                      } else if (translationY > translationThreshold ) {
-                        animate(up);
-                      }
-                    }
-                    
-                  }
-              }
-            });
-
-            controller.connect();
-
-            function animate(direction) {
-              switch(direction){
-                case left:
-                  slide(columns, posibleXPositions, false); // left
-                  break;
-                case up:
-                  slide(rows, posibleYPositions, true); // up
-                  break;
-                case right:
-                  slide(columns, posibleXPositions, true); // right
-                  break;
-                case down:
-                  slide(rows, posibleYPositions, false); // down
-                  break;
-              }
-              animating = true;
-
-              window.setTimeout(function(){
-                animating = false;
-              }, 500);
-
-            }
-
-            var $body = $(ns.body);
-
-            $body.on("keydown", function(key){
-              switch(key.keyCode){
-                case 37:
-                  animate(left);
-                  break;
-                case 38:
-                  animate(up);
-                  break;
-                case 39:
-                  animate(right);
-                  break;
-                case 40:
-                  animate(down);
-                  break;
-                case 90:
-                  $(".frame").toggleClass("zoomed-out");
-                  break;
-              }
-            });
+        // Just for Debugging 
+        $(ns.body).on("keydown", function(key){
+          switch(key.keyCode){
+            case 37:
+              animate(left);
+              break;
+            case 38:
+              animate(up);
+              break;
+            case 39:
+              animate(right);
+              break;
+            case 40:
+              animate(down);
+              break;
+            case 90:
+              $(".frame").toggleClass("zoomed-out");
+              break;
+          }
+        });
       };
 
-        init();
+      init();
     };
 
 })(window.Caviar);
